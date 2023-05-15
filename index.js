d07689a085979ea69b9af9f524e4e3cc209a043a
Yonashenok <yonashenok24@gmail.com>
2023-05-15 16:34:38 +0300

    added scroll in to view effect

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const hamburgerMenu = document.querySelector('.menu_section');
-const logoHem = document.querySelector('.hamburgermenu');
 const logoCross = document.querySelector('.crossSign');
+const logoHem = document.querySelector('.hamburgermenu');
 const navBar = document.querySelector('.menu_m');
 const logo = document.querySelector('.navLogo');
+const navMenus = document.querySelectorAll('.close-menu');
 
-const menuHandler = () => {
+const toggleMenuHandler = (event) => {
+  event.preventDefault();
   logoCross.classList.toggle('hidden');
   logoHem.classList.toggle('hidden');
   navBar.classList.toggle('hidden');
   logo.classList.toggle('hidden');
 };
 
-hamburgerMenu.addEventListener('click', menuHandler);
+const scrollHandler = (event) => {
+  event.preventDefault();
+  if (event.target.classList.contains('close-menu')) {
+    const id = event.target.getAttribute('href');
+    document
+      .querySelector(id)
+      .scrollIntoView({ behavior: 'smooth', inline: 'nearest' });
+  }
+};
+
+hamburgerMenu.addEventListener('click', toggleMenuHandler);
+
+navMenus.forEach((menu) => {
+  menu.addEventListener('click', toggleMenuHandler);
+});
+
+navMenus.forEach((menu) => {
+  menu.addEventListener('click', scrollHandler);
+});
