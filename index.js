029853d3ec5dd4d767600ed135513d8973fef3d0
Yonashenok <yonashenok24@gmail.com>
2023-05-16 18:56:54 +0300

    added array object data and close popup handler

--- index.js
+++ index.js
@@ -4,62 +4,64 @@ const logoHem = document.querySelector('.hamburgermenu');
 const navBar = document.querySelector('.menu_m');
 const logo = document.querySelector('.navLogo');
 const popups = document.querySelector('.popup');
-const showPopups = document.querySelector('.show-project');
+const overly = document.querySelector('.overly');
+const showPopups = document.querySelectorAll('.show-project');
 const logoCross = document.querySelector('.crossSign');
 
+let closePopUp;
 const projectData = [
   {
-    id: 1,
+    id: '1',
     title: 'Multi-Post Stories',
     description:
       ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum neque dolorem voluptatum. Eum adipisci omnis dolores ab est nobis at   repudiandae consequatur eaque dolorem! Nostrum dolorem aspernatur debitis perferendis quis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae iste ipsa error esse eum veritatis, nihil',
     techStack: { tech1: 'HTML', tech2: 'Bootstrap', tech3: 'Ruby on rails' },
-    img: '',
+    img: './style/imgs/Snapshoot_mobile.png',
     link: '',
   },
   {
-    id: 2,
+    id: '2',
     title: 'Professional Art Printing Data',
     description:
       ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum neque dolorem voluptatum. Eum adipisci omnis dolores ab est nobis at   repudiandae consequatur eaque dolorem! Nostrum dolorem aspernatur debitis perferendis quis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae iste ipsa error esse eum veritatis, nihil',
     techStack: { tech1: 'HTML', tech2: 'Bootstrap', tech3: 'Ruby on rails' },
-    img: '',
+    img: './style/imgs/Snapshoot_mobile.png',
     link: '',
   },
   {
-    id: 3,
+    id: '3',
+    title: 'Data Dashboard Healthcare',
+    description:
+      ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum neque dolorem voluptatum. Eum adipisci omnis dolores ab est nobis at   repudiandae consequatur eaque dolorem! Nostrum dolorem aspernatur debitis perferendis quis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae iste ipsa error esse eum veritatis, nihil',
+    techStack: { tech1: 'HTML', tech2: 'Bootstrap', tech3: 'Ruby on rails' },
+    img: 's',
+    link: './style/imgs/Snapshoot_mobile.png',
+  },
+  {
+    id: '4',
     title: 'Professional Art Printing Data',
     description:
       ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum neque dolorem voluptatum. Eum adipisci omnis dolores ab est nobis at   repudiandae consequatur eaque dolorem! Nostrum dolorem aspernatur debitis perferendis quis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae iste ipsa error esse eum veritatis, nihil',
     techStack: { tech1: 'HTML', tech2: 'Bootstrap', tech3: 'Ruby on rails' },
-    img: '',
+    img: './style/imgs/Snapshoot_mobile.png',
     link: '',
   },
   {
-    id: 4,
-    title: 'Professional Art Printing Data',
+    id: '5',
+    title: 'Data Dashboard Healthcare',
     description:
       ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum neque dolorem voluptatum. Eum adipisci omnis dolores ab est nobis at   repudiandae consequatur eaque dolorem! Nostrum dolorem aspernatur debitis perferendis quis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae iste ipsa error esse eum veritatis, nihil',
     techStack: { tech1: 'HTML', tech2: 'Bootstrap', tech3: 'Ruby on rails' },
-    img: '',
+    img: './style/imgs/Snapshoot_mobile.png',
     link: '',
   },
   {
-    id: 5,
-    title: 'Professional Art Printing Data',
+    id: '6',
+    title: 'Data Dashboard Healthcare',
     description:
       ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum neque dolorem voluptatum. Eum adipisci omnis dolores ab est nobis at   repudiandae consequatur eaque dolorem! Nostrum dolorem aspernatur debitis perferendis quis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae iste ipsa error esse eum veritatis, nihil',
     techStack: { tech1: 'HTML', tech2: 'Bootstrap', tech3: 'Ruby on rails' },
-    img: '',
-    link: '',
-  },
-  {
-    id: 6,
-    title: 'Professional Art Printing Data',
-    description:
-      ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum neque dolorem voluptatum. Eum adipisci omnis dolores ab est nobis at   repudiandae consequatur eaque dolorem! Nostrum dolorem aspernatur debitis perferendis quis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae iste ipsa error esse eum veritatis, nihil',
-    techStack: { tech1: 'HTML', tech2: 'Bootstrap', tech3: 'Ruby on rails' },
-    img: '',
+    img: './style/imgs/Snapshoot_mobile.png',
     link: '',
   },
 ];
@@ -72,6 +74,10 @@ const toggleMenuHandler = (event) => {
   logo.classList.toggle('hidden-g');
 };
 
+const closePopUpHandler = (event) => {
+  event.preventDefault();
+  overly.classList.add('hidden-g');
+};
 const scrollHandler = (event) => {
   event.preventDefault();
   if (event.target.classList.contains('close-menu')) {
@@ -84,44 +90,62 @@ const scrollHandler = (event) => {
 
 const renderPopUp = (e) => {
   e.preventDefault();
-
-  const markUp = `
-  <h2>Multi-Post Stories</h2>
-        <ul class="skills-lan flex-row">
-          <li><a class="tag" href="#">HTML</a></li>
-          <li><a class="tag" href="#">Bootstrap</a></li>
-          <li><a class="tag" href="#">Ruby on rails</a></li>
-        </ul>
-        <div class="bg-img"></div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum neque
-          dolorem voluptatum. Eum adipisci omnis dolores ab est nobis at
-          repudiandae consequatur eaque dolorem! Nostrum dolorem aspernatur
-          debitis perferendis quis. Lorem ipsum dolor, sit amet consectetur
-          adipisicing elit. Vitae iste ipsa error esse eum veritatis, nihil
-          minima, sunt culpa alias odio magnam numquam, eius id qui? Repudiandae
-          numquam beatae magnam?
-        </p>
-        <ul class="popup-btn flex-row">
-          <li class="flex-row popup-cont">
-            <a href="#">See live</a>
-            <img
-              class="popup-icon"
-              src="./style/imgs/Icon.png"
-              alt="live-share-icon"
-            />
-          </li>
-          <li class="flex-row popup-cont">
-            <a href="#">See source</a>
-            <img
-              class="popup-icon"
-              src="./style/imgs/Group.png"
-              alt="source code to github"
-            />
-          </li>
-      </ul>`;
+  popups.innerHTML = '';
+  overly.classList.remove('hidden-g');
+  const id = e.target.dataset.show;
+  const [templateMakeup] = projectData.filter((item) => item.id === id);
+  const markUp = ` 
+  <div class="flex-row ">
+  <h2>${templateMakeup.title}</h2>
+<svg
+xmlns="http://www.w3.org/2000/svg"
+fill="none"
+viewBox="0 0 24 24"
+stroke-width="1.5"
+height="32"
+width="32"
+stroke="#000"
+class="w-6 h-6 crossIcon"
+>
+<path
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  d="M6 18L18 6M6 6l12 12"
+/>
+</svg>
+</div>
+  <ul3 class="skills-lan flex-row">
+  <li><a class="tag" href="#">  ${templateMakeup.techStack.tech1}</a></li>
+  <li><a class="tag" href="#">  ${templateMakeup.techStack.tech2}</a></li>
+  <li><a class="tag" href="#">  ${templateMakeup.techStack.tech3}</a></li>
+  </ul3
+  <div class="bg-img"><img class="projectsnap" src=${templateMakeup.img} alt="project screen shoot"> </div>
+  <p>
+      ${templateMakeup.description}
+      </p>
+      <ul class="popup-btn flex-row">
+        <li class="flex-row popup-cont">
+        <a href="#">See live</a>
+        <img
+        class="popup-icon"
+        src="./style/imgs/Icon.png"
+        alt="live-share-icon"
+        />
+        </li>
+        <li class="flex-row popup-cont">
+        <a href="#">See source</a>
+        <img
+        class="popup-icon"
+        src="./style/imgs/Group.png"
+        alt="source code to github"
+        />
+        </li>
+        </ul>`;
   popups.insertAdjacentHTML('afterbegin', markUp);
+  closePopUp = document.querySelector('.crossIcon');
+  closePopUp.addEventListener('click', closePopUpHandler);
 };
+
 showPopups.forEach((btn) => {
   btn.addEventListener('click', renderPopUp);
 });
