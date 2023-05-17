30d3958aa704e6fdce5c5e5ba9c044300e950109
Yonashenok <yonashenok24@gmail.com>
2023-05-17 09:52:25 +0300

    added popup window for desktop version

--- index.js
+++ index.js
@@ -7,12 +7,11 @@ const popups = document.querySelector('.popup');
 const overly = document.querySelector('.overly');
 const showPopups = document.querySelectorAll('.show-project');
 const logoCross = document.querySelector('.crossSign');
-
 let closePopUp;
 const projectData = [
   {
     id: '1',
-    title: 'Multi-Post Stories',
+    title: 'Professional Art Printing Data',
     description:
       ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum neque dolorem voluptatum. Eum adipisci omnis dolores ab est nobis at   repudiandae consequatur eaque dolorem! Nostrum dolorem aspernatur debitis perferendis quis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae iste ipsa error esse eum veritatis, nihil',
     techStack: { tech1: 'HTML', tech2: 'Bootstrap', tech3: 'Ruby on rails' },
@@ -34,8 +33,8 @@ const projectData = [
     description:
       ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum neque dolorem voluptatum. Eum adipisci omnis dolores ab est nobis at   repudiandae consequatur eaque dolorem! Nostrum dolorem aspernatur debitis perferendis quis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae iste ipsa error esse eum veritatis, nihil',
     techStack: { tech1: 'HTML', tech2: 'Bootstrap', tech3: 'Ruby on rails' },
-    img: 's',
-    link: './style/imgs/Snapshoot_mobile.png',
+    img: './style/imgs/Snapshoot_mobile.png',
+    link: '',
   },
   {
     id: '4',
@@ -61,7 +60,7 @@ const projectData = [
     description:
       ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum neque dolorem voluptatum. Eum adipisci omnis dolores ab est nobis at   repudiandae consequatur eaque dolorem! Nostrum dolorem aspernatur debitis perferendis quis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae iste ipsa error esse eum veritatis, nihil',
     techStack: { tech1: 'HTML', tech2: 'Bootstrap', tech3: 'Ruby on rails' },
-    img: './style/imgs/Snapshoot_mobile.png',
+    img: './style/imgs/popup.png',
     link: '',
   },
 ];
@@ -95,52 +94,72 @@ const renderPopUp = (e) => {
   const id = e.target.dataset.show;
   const [templateMakeup] = projectData.filter((item) => item.id === id);
   const markUp = ` 
-  <div class="flex-row ">
+  <div class="flex-row">
   <h2>${templateMakeup.title}</h2>
-<svg
-xmlns="http://www.w3.org/2000/svg"
-fill="none"
-viewBox="0 0 24 24"
-stroke-width="1.5"
-height="32"
-width="32"
-stroke="#000"
-class="w-6 h-6 crossIcon"
->
-<path
-  stroke-linecap="round"
-  stroke-linejoin="round"
-  d="M6 18L18 6M6 6l12 12"
-/>
-</svg>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    height="32"
+    width="32"
+    stroke="#67798E"
+    class="w-6 h-6 crossIcon"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M6 18L18 6M6 6l12 12"
+    />
+  </svg>
 </div>
-  <ul3 class="skills-lan flex-row">
-  <li><a class="tag" href="#">  ${templateMakeup.techStack.tech1}</a></li>
-  <li><a class="tag" href="#">  ${templateMakeup.techStack.tech2}</a></li>
-  <li><a class="tag" href="#">  ${templateMakeup.techStack.tech3}</a></li>
-  </ul3
-  <div class="bg-img"><img class="projectsnap" src=${templateMakeup.img} alt="project screen shoot"> </div>
-  <p>
-      ${templateMakeup.description}
-      </p>
-      <ul class="popup-btn flex-row">
-        <li class="flex-row popup-cont">
+<ul class="skills-lan flex-row">
+  <li><a class="tag" href="#">${templateMakeup.techStack.tech1}</a></li>
+  <li><a class="tag" href="#">${templateMakeup.techStack.tech2}</a></li>
+  <li><a class="tag" href="#">${templateMakeup.techStack.tech3}</a></li>
+</ul>
+<div class="popupContainer">
+  <div class="bg-img">
+    <img
+      class="projectsnap"
+      src=${templateMakeup.img}
+      alt="project screen shoot"
+    />
+  </div>
+  <div class="popupText">
+    <p>
+    ${templateMakeup.description}
+    </p>
+
+    <p class="text-box hidden-m">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est ex ipsam
+    eligendi impedit, laudantium officia exercitationem non error ab
+    corrupti modi veniam, laborum eum earum vitae voluptates fugiat, sunt
+    aut. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam
+    magnam facilis ex, est recusandae non fugiat odio ipsum ipsam iste ab
+    nostrum aperiam dolor? Modi doloremque beatae voluptatum maxime hic!  
+    </p>
+    <ul class="popup-btn flex-row">
+      <li class="flex-row popup-cont">
         <a href="#">See live</a>
         <img
-        class="popup-icon"
-        src="./style/imgs/Icon.png"
-        alt="live-share-icon"
+          class="popup-icon"
+          src="./style/imgs/Icon.png"
+          alt="live-share-icon"
         />
-        </li>
-        <li class="flex-row popup-cont">
+      </li>
+      <li class="flex-row popup-cont">
         <a href="#">See source</a>
         <img
-        class="popup-icon"
-        src="./style/imgs/Group.png"
-        alt="source code to github"
+          class="popup-icon"
+          src="./style/imgs/Group.png"
+          alt="source code to github"
         />
-        </li>
-        </ul>`;
+      </li>
+    </ul>
+  </div>
+</div>
+  `;
   popups.insertAdjacentHTML('afterbegin', markUp);
   closePopUp = document.querySelector('.crossIcon');
   closePopUp.addEventListener('click', closePopUpHandler);
